fb8362268dae899e5f1ccb7e2724882b4432db00
Guy John <guy@notesandvolts.com>
2010-02-24 23:28:59 +0000

    Major changes to server. need a rethink of how to keep connections open and contact nodes

--- server.py
+++ server.py
@@ -6,25 +6,43 @@ import socket
 import threading
 import Queue
 
-class serverObj ():
+jobQueue = Queue.Queue(0)
+lock = thread.allocate_lock()
 
-    def __init__(self, configFile):
-        self.nodeIPs = 0
-        self.nodeThreads = 0
+
+class ServerObj():
+
+    def __init__(self):
+        self.serverIP = None
+        self.basePort = 5007
+        self.baseSocket = None
+        
+        self.threadNumber = 0
         self.jobQueue = 0
+        
+        self.startingUp  = True
         self.taskRunning = False
-        # load config file, create array of node IPs
     
-    def CheckNodes(self):
-        pass
-        # run through array of IPs, check connections and nodes are ok
-    
-    def CreateThreads(self):
-        self.taskRunning = True
-        # creates a thread for each IP in the node list
-        # sets the task running as it creates each node
-    
-    def SplitJob(self, numberPerNode, Dimensions):
+    def CreateSocket(self):
+        while self.startingUp:
+            try:
+                self.baseSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            except socket.error, msg:
+                continue
+            
+            try:
+                self.baseSocket.bind((self.serverIP,self.basePort))
+                self.baseSocket.listen(5)
+            except socket.error, msg:
+                self.baseSocket.close()
+                self.baseSocket = None
+                continue
+                
+            self.baseSocket.settimeout(5)
+            self.startingUp = False
+            self.taskRunning = True
+
+    def CreateJobs(self, numberPerNode, Dimensions):
         pass
         # splits the job up into a number of sub tasks
         # essentially just splits up the povray picture dimensions
@@ -33,24 +51,63 @@ class serverObj ():
     def JoinImages(self):
         pass
         # joins the image slices together
+ 
 
-class NodeThread( threading.Thread ):
+class NodeThread(threading.Thread):
 
-    def __init__(self, IPAdr, Port):
-        self.NodeIPs = 0
-        self.socket
-        self.working = 0
-        # load config file, create array of node IPs
+    def __init__(self, nodeSocket):
+        self.node = NodeObject(nodeSocket)
+        self.running = True
+        
+        threading.Thread.__init__.self
+        # create a thread for a particular socket
     
-    def SendFile(self, filename):
+    def run(self):
+    
+        lock.acquire()
+        ClusterServer.threadnumber += 1
+        lock.release()
+        
+        self.node.sendFiles()
+        self.node.HandShake()
+        while self.running:
+            self.node.RunTask()
+            self.node.WaitForTask()
+            self.node.RetrieveImage()
+        
+        self.node.CleanupNode()
+        lock.acquire()
+        ClusterServer.threadnumber -= 1
+        if ClusterServer.threadnumber == 0:
+            ClusterServer.taskRunning = False
+        lock.release()
+
+class NodeObject()
+
+    def __init__(self, nodeSocket):
+        self.socket = nodeSocket
+
+    def SendFiles(self, filename):
         pass
-        # send a file to each of the nodes
+        # send a file to the node
+    
+    def RunTask(self):
+        pass
+        # get a job from the queue and send it to the node
+
+    def HandShake(self):
+        pass
+        # check that the node is ready to go
+
+    def WaitForTask(self):
+        pass
+        # wait for the task to be completed
 
     def RetrieveImage(self):
         pass
-        # retrieves an image from a node
+        # retrieve an image from a node
     
-    def CleanupNode(self, nodeNumber):
+    def CleanupNode(self):
         pass
         # deletes any files off nodes
 
@@ -61,13 +118,21 @@ if __name__ == '__main__':
     width     = 1024
     height    = 768
 
-    ClusterServer = serverObj (configFile)
-    ClusterServer.CheckNodes()
-    ClusterServer.SplitJob(nodeTasks, (width, height))
-    ClusterServer.CreateThreads()
+    ClusterServer = ServerObj()
+    global ClusterServer
+    
+    ClusterServer.CreateJobs(nodeTasks, (width, height))
+    
+    ClusterServer.CreateSocket()
 
     while ClusterServer.taskRunning:
-        pass
+        try:
+            socket = ClusterServer.baseSocket.accept()
+            socket.settimeout(None)
+            NodeThread(socket).start()
+        except socket.timeout
+            pass
+
 
 
     ClusterServer.JoinImages()
