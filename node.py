27b1a1e8c361b8f45b60f78e0102da7fca00a278
Guy John <guy@notesandvolts.com>
2010-02-26 05:13:22 -0700

    Reasonably finalised design for nodes. Might possibly wat to add in a thread to keep in communication with server whilst job is running but not necesary yet

--- node.py
+++ node.py
@@ -3,84 +3,99 @@
 #Import Modules
 import os
 import socket
+import time
 
 class NodeObj ():
 
     def __init__(self):
+        # details of main server
+        # will eventually be in cfg file
+        # possibly host name based
         self.serverIP        = "192.168.0.50"
         self.serverPort      = 6007
-        self.jobPort         = None
 
-        self.wakeSocket      = None
-        self.jobSocket       = None
+        # network socket connected to server
+        self.serverSocket    = None
 
+        # command line params and file list
         self.jobParams       = None
-        self.jobFile         = None
+        self.jobFiles        = None
 
-        self.findingServer   = True
-        self.connectingJob   = False
+        # node states
         self.connectedServer = False
         self.taskRunning     = False
 
-    def GetJobfile(self):
-        pass
-        # send a request for the jobFile
+    def ServerConnect(self):
+        # keep trying to connect to server
+        while !self.connectedServer:
+            try:
+                self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            except socket.error, msg:
+                time.sleep(2)
+                continue
 
-    def RunProcess(self):
+            try:
+                self.serverSocket.connect((self.serverIP,self.serverPort))
+            except socket.error, msg:
+                self.serverSocket.close()
+                self.serverSocket = None
+                time.sleep(2)
+                continue
+
+            self.serverSocket.setblocking(1)
+            self.connectedServer = True
+        
+        return 1
+
+    def Handshake(self):
+        # check in with the server
+        # tell it that this is a node
+        waiting = True
+        while waiting:
+            self.wakeSocket.send('node')
+            serverStatus = self.wakeSocket.recv(1024)
+            if serverStatus == 'connected':
+                waiting = False
+            else:
+                time.sleep(2)
+                
+        return serverStatus
+
+    def CheckForJobs(self):
+        while checking
+            self.wakeSocket.send('work')
+            jobStatus = self.wakeSocket.recv(1024)
+            if jobStatus == 'available':
+                waiting = False
+            else:
+                time.sleep(2)
+
+    def GetJobInfo(self):
         pass
+        #get the job files and params
+
+    def RunJob(self):
         # run the job process
-
-    def SendStatus(self):
         pass
-        # send the status of the job to the server
-        # called when the job finishes
 
     def CleanUp(self):
-        pass
         # deletes all local files
         # closes job listening socket
+        pass
 
 
 if __name__ == '__main__':
 
     clusterNode = NodeObj()
 
-    while clusterNode.findingServer:
-        try:
-            clusterNode.wakeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error, msg:
-            continue
-
-        try:
-            clusterNode.wakeSocket.connect((clusterNode.serverIP,clusterNode.serverPort))
-        except socket.error, msg:
-            clusterNode.wakeSocket.close()
-            clusterNode.wakeSocket = None
-            continue
-
-        clusterNode.findingServer = False
-        clusterNode.connectingJob = True
-        clusterNode.jobPort = clusterNode.wakeSocket.recv(1024)
-
-
-    while clusterNode.connectingJob:
-
-        try:
-            clusterNode.jobSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error, msg:
-            continue
-
-        try:
-            clusterNode.jobSocket .connect((clusterNode.serverIP,clusterNode.jobPort))
-        except socket.error, msg:
-            clusterNode.jobSocket .close()
-            clusterNode.jobSocket = None
-            continue
-
-        clusterNode.connectingJob = False
-        clusterNode.connectedServer = True
-
-    while self.connectedServer:
+    while true:
+        clusterNode.ServerConnect()
+        clusterNode.Handshake()
         
-        
+        while clusterNode.connectedServer:
+            clusterNode.CheckForJobs()
+            
+            while clusterNode.taskRunning:
+                clusterNode.RunJob()
+            
 
