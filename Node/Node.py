db38c1484cf1953dc40c3793a0483a9db5d3851d
Guy John <d.guyjohn@gmail.com>
2011-07-26 18:59:49 -0400

    Adding Node class
    basically just stubbed out for the moment

---
+++ Node/Node.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from RayManager import RayManager
+
+class Node():
+
+    def __init__():
+        self.manager = RayManager()
+        self.coms = None
+
+    def check_for_jobs():
+        pass
+
+    def new_job():
+        pass
+
+    def check_processes():
+        pass
+
+    def good_job():
+        pass
+
+    def bad_job():
+        pass
+
+    def event_loop():
+        pass
+
+
