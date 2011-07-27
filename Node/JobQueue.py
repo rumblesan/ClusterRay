7b110c6cb46075d61913aeb5a9ff266846855659
Guy John <d.guyjohn@gmail.com>
2011-07-27 18:42:14 -0400

    Adding JobQueue class
    just stubbed out at the momment

---
+++ Node/JobQueue.py
@@ -0,0 +1,17 @@
+
+
+class JobQueue():
+
+    jobs = None
+
+    def __init__():
+        pass
+
+    def jobs_available():
+        pass
+
+    def job_completed(job_info):
+        pass
+
+    def job_errored(job_info):
+        pass
