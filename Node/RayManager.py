a5c85aef3b3a9d62705c77bbe22693f1c172d5d9
Guy John <d.guyjohn@gmail.com>
2011-07-27 18:44:38 -0400

    Updating RayManager to inherit from Manager class

--- Node/RayManager.py
+++ Node/RayManager.py
@@ -1,15 +1,14 @@
 
 from Povray import Povray
+from Manager import Manager
 
-class RayManager():
+class RayManager(Manager):
 
-    process_list = []
-    process_num  = 1
     debug        = False
-    running      = 0
 
-    def __init__(self, debug):
+    def __init__(self, max_rays=1, debug=False):
         self.debug = debug
+        Manager.__init__(self, max_rays)
 
     def new_process(self, config_json):
         new_process = Povray(config_json, self.process_num)
@@ -17,26 +16,21 @@ class RayManager():
         new_process.create_args()
         new_process.run()
         self.process_list.append(new_process)
-        if self.debug:
-            print "Process %i added" % self.process_num
-        self.process_num += 1
-        self.running += 1
+        self.add_task(new_process)
 
     def check_processes(self):
         for process in self.process_list:
             poll_value = process.poll()
-            if self.debug:
-                print "Process %i poll is %s" % (process.node_number, poll_value)
 
     def clear_processes(self):
-        finished = [process for process in self.process_list if process.completed]
         self.process_list = [process for process in self.process_list if not process.completed]
         self.running -= len(finished)
-        if self.debug:
-            print "%i processes finished" % len(finished)
         for process in finished:
             print "%i  %s" % (process.node_number, process.get_image())
         if self.debug:
             print "%i processes still running" % len(self.process_list)
+
+    def finished(self, task):
+        return task.completed
 
 
