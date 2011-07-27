b9481ca4a5312c8d6837d735270b113bf5cc26b9
Guy John <d.guyjohn@gmail.com>
2011-07-27 18:37:01 -0400

    Added JobManager class
    used for keeping track of jobs and creating new Povray instances in the RayManager

---
+++ Node/JobManager.py
@@ -0,0 +1,18 @@
+
+from RayManager import RayManager
+from Manager import Manager
+
+class JobManager():
+
+    debug        = False
+
+    def __init__(self, max_jobs=1, debug=False):
+        self.debug = debug
+        Manager.__init__(self, max_jobs)
+
+    def add_job(self, job_info):
+        pass
+
+    def check_jobs(self):
+        pass
+
