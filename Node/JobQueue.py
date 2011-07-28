5ac2ddee19eb8d2185dcb5f26230044bfcf695e0
Guy John <d.guyjohn@gmail.com>
2011-07-28 18:29:29 -0400

    Started filling in JobQueue
    still not much there other than placeholder methods

--- Node/JobQueue.py
+++ Node/JobQueue.py
@@ -2,13 +2,20 @@
 
 class JobQueue():
 
-    jobs = None
+    jobs = []
 
     def __init__():
         pass
 
-    def jobs_available():
-        pass
+    def jobs_available(self):
+        if len(self.jobs) == 0:
+            return False
+        else:
+            return True
+
+    def get_job(self):
+        if not len(self.jobs) == 0:
+            return self.jobs.pop(0)
 
     def job_completed(job_info):
         pass
