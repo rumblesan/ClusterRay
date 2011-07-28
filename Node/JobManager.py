5d4e172d3aca1dcf751262981d6a670aeb5893c6
Guy John <d.guyjohn@gmail.com>
2011-07-28 18:30:17 -0400

    JobManager adds jobs and checks finished jobs
    passes info back a dictionary for each task giving task_id, output_filename and status

--- Node/JobManager.py
+++ Node/JobManager.py
@@ -1,18 +1,42 @@
 
 from RayManager import RayManager
-from Manager import Manager
+import os
 
-class JobManager(Manager):
+class JobManager():
 
-    debug        = False
+    debug          = False
+    RayManager     = None
+    job_info       = {}
 
     def __init__(self, max_jobs=1, debug=False):
         self.debug = debug
-        Manager.__init__(self, max_jobs)
+        self.RayManager = RayManager(max_jobs, debug)
 
     def add_job(self, job_info):
-        pass
+        job_info['outputfile'] = str(job_info['job_id'])
+        job_number = self.RayManager.new_process(job_info)
+        self.job_info[job_number] = job_info
 
-    def check_jobs(self):
-        pass
+    def get_finished_jobs(self):
+        finished_jobs = self.RayManager.get_finished_tasks()
+        job_list = []
+        if len(finished_jobs) > 0:
+            for id_num, job in self.RayManager.finished_tasks.iteritems():
+                job_return = {}
+                job_return['job_id']   = self.job_info[key]['job_id']
+                if task.return_value == 0:
+                    job_return['status'] = 'OK'
+                    job_return['job_file'] = job.get_image()
+                else:
+                    file_name = job.get_image()
+                    if os.path.exists(file_name):
+                        os.remove(file_name)
+                    job_return['status'] = 'ERROR'
+                job_list.append(job_return)
+        return job_list
+
+
+
+    def free_job_slots(self):
+        return self.RayManager.free_task_slots()
 
