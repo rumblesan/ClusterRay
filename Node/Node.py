d5e2fa11e15a4c97e14b46c7394ce77407b8acf4
Guy John <d.guyjohn@gmail.com>
2011-07-28 18:32:54 -0400

    started filling out main Node class

--- Node/Node.py
+++ Node/Node.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 
 from RayManager import RayManager
-from NodeComs import NodeComs
+from JobQueue import JobQueue
 
 class Node():
 
     def __init__(self):
-        self.manager   = JobManager()
-        self.job_queue = JobQueue()
+        self.JobManager = JobManager()
+        self.JobQueue   = JobQueue()
 
     def check_for_jobs(self):
         #checks the JobQueue object for new jobs
-        pass
-
-    def new_job(self):
-        #adds a new job to the JobManager object
-        pass
+        if self.JobQueue.jobs_available():
+            job_info = self.JobQueue.get_job()
+            self.JobManager.add_job(job_info)
 
     def get_finished_jobs(self):
         #get finished jobs from JobManager
-        pass
+        finished = self.JobManager.get_finished_jobs()
+        for job in finished:
+            if job['status'] == 'OK':
+                self.good_job(job)
+            else:
+                self.bad_job(job)
 
-    def good_job(self):
+    def good_job(self, job):
         #if those processes finished ok, send back the info
-        pass
+        print job
 
-    def bad_job(self):
+    def bad_job(self, job):
         #if those processes didn' finished ok, alert the Master
-        pass
+        print job
 
 
