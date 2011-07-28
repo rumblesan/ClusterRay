d7ba316af93f22796d43c7375fd59446d0835c58
Guy John <d.guyjohn@gmail.com>
2011-07-28 18:28:38 -0400

    RayManager simplified
    realised that much of the stuff is handled by the Manager class inheritance

--- Node/RayManager.py
+++ Node/RayManager.py
@@ -12,25 +12,14 @@ class RayManager(Manager):
 
     def new_process(self, config_json):
         new_process = Povray(config_json, self.process_num)
-        new_process.setup()
         new_process.create_args()
         new_process.run()
         self.process_list.append(new_process)
-        self.add_task(new_process)
+        ray_number = self.add_task(new_process)
+        return ray_number
 
-    def check_processes(self):
-        for process in self.process_list:
-            poll_value = process.poll()
-
-    def clear_processes(self):
-        self.process_list = [process for process in self.process_list if not process.completed]
-        self.running -= len(finished)
-        for process in finished:
-            print "%i  %s" % (process.node_number, process.get_image())
-        if self.debug:
-            print "%i processes still running" % len(self.process_list)
-
-    def finished(self, task):
-        return task.completed
+    def finished(self, process):
+        process.poll()
+        return process.completed
 
 
