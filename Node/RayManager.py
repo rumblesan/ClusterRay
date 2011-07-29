0b9ba78ae422495da9f96436ad5b7aaf0194efdb
Guy John <d.guyjohn@gmail.com>
2011-07-29 19:53:08 -0400

    Correctly adds a Povray Process
    uses add_task method from Manager class correctly
    Doesn't pass Povray process a node_number because it's not needed

--- Node/RayManager.py
+++ Node/RayManager.py
@@ -11,10 +11,9 @@ class RayManager(Manager):
         Manager.__init__(self, max_rays)
 
     def new_process(self, config_json):
-        new_process = Povray(config_json, self.process_num)
+        new_process = Povray(config_json)
         new_process.create_args()
         new_process.run()
-        self.process_list.append(new_process)
         ray_number = self.add_task(new_process)
         return ray_number
 
