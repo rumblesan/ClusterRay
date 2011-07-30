cd8636257949cf5c071fb7dd6fa1a3b0305f337a
Guy John <d.guyjohn@gmail.com>
2011-07-29 20:13:53 -0400

    Updated Node object
    Now correctly checks for and adds new jobs
    also has main loop within the program

--- Node/Node.py
+++ Node/Node.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 
-from RayManager import RayManager
+from JobManager import JobManager
 from JobQueue import JobQueue
 
 class Node():
 
     def __init__(self):
-        self.JobManager = JobManager()
+        self.JobManager = JobManager(4)
         self.JobQueue   = JobQueue()
+
+    def free_job_slots(self):
+        return self.JobManager.free_job_slots()
 
     def check_for_jobs(self):
         #checks the JobQueue object for new jobs
         if self.JobQueue.jobs_available():
             job_info = self.JobQueue.get_job()
-            self.JobManager.add_job(job_info)
+            self.add_job(job_info)
+
+    def add_job(self, new_job):
+        self.JobManager.add_job(new_job)
 
     def get_finished_jobs(self):
         #get finished jobs from JobManager
@@ -32,4 +38,18 @@ class Node():
         #if those processes didn' finished ok, alert the Master
         print job
 
+    def heartbeat(self):
+        #sends message back to Master to say Node is still alive
+        print 'heartbeat'
 
+def main():
+    node = Node()
+    while 1:
+        if node.free_job_slots():
+            node.check_for_jobs()
+        node.get_finished_jobs()
+        node.heartbeat()
+        sleep(5)
+
+if __name__ == '__main__':
+    main()
