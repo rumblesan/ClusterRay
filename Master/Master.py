e218e1f6eb84189cbdfb22a8971be803f1bf1ac5
Guy John <d.guyjohn@gmail.com>
2011-08-07 17:47:01 -0400

    More work on Master class
    added function to register message action callbacks. have a better understanding of the logic flow involved

--- Master/Master.py
+++ Master/Master.py
@@ -2,35 +2,46 @@
 
 class Master():
 
-    def __init__(self, node_coms, jobmanager):
-        self.jobmanager = jobmanager
-        self.node_coms  = node_coms
+    def __init__(self, node_coms, client_coms, node_manager, job_manager):
+        self.job_manager  = job_manager
+        self.node_coms    = node_coms
+        self.node_manager = node_manager
+        self.client_coms  = client_coms
+
+        self.message_actions()
+
+    def message_actions(self):
+        #this function deals with the request actions
+        #message action are:-
+        #    HANDSHAKE
+        #    NEWTASK
+        #    HEARTBEAT
+        #    FINISHED
+        actions = {}
+        actions['NEWTASK'] = self.job_manager.get_job
+        actions['FINISHED'] = self.job_manager.finished_job
+        #actions['HEARTBEAT'] = 
+        #actions['HANDSHAKE'] = 
+        for name, function in actions.iteritems():
+            self.node_coms.register_action(name, function)
 
     def check_for_jobs(self):
         #checks for incoming jobs that will be split into
         #tasks and distributed to the Nodes
-        pass
+        self.client_coms.get_jobs()
 
-    def add_job(self, task_info):
+    def add_job(self, job_info):
         #add a new job to the job manager
         #job manager deals with splitting it up into tasks
-        pass
+        self.job_manager.add_job(job_info)
 
-    def get_finished_tasks(self):
-        #get finished tasks from the Nodes
-        #give them to the job manager
-        pass
-
-    def get_finished_jobs(self):
-        #check the job manager for finished jobs
-        #a job will be finished once the job manager has
-        #all the tasks assigned to it
-        pass
+    def get_messages(self):
+        #gets any messages sent by the Nodes
+        self.node_coms.check()
 
     def check_nodes(self):
-        #check that the nodes have sent heartbeat messages
-        #if any haven't for too long then kill that Node
-        #then send its jobs out to other Nodes
-        pass
+        #checks the node manager to make sure all Nodes
+        #are still functioning
+        self.node_coms.check()
 
 
