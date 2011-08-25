23677891d0973657c4d4363c81bc642d7a529ef5
Guy John <d.guyjohn@gmail.com>
2011-08-25 15:00:36 -0400

    Restubbing out JobManager
    Had a sit down and a think about how this should really work

---
+++ Master/JobManager.py
@@ -0,0 +1,24 @@
+
+from libs.PovJob import PovJob
+
+class JobManager():
+
+    running_jobs = {}
+    new_jobs     = []
+
+    def __init__(self):
+        pass
+
+    def new_job(self, job_info):
+        pass
+
+    def activate_job(self):
+        pass
+
+    def finished_task(self, task_info):
+        pass
+
+    def get_finished_jobs(self):
+        pass
+
+
