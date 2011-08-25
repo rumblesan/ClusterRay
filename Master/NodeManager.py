f558e5ace5c84e0b48082f6e682ef1ad4d777d50
Guy John <d.guyjohn@gmail.com>
2011-08-25 15:26:27 -0400

    Adding stubbed out NodeManager

---
+++ Master/NodeManager.py
@@ -0,0 +1,15 @@
+
+class NodeManager():
+
+    nodes     = {}
+    node_jobs = {}
+
+    def new_node(self, node_id):
+        pass
+
+    def register_task(self, node_id, task_id)
+        pass
+
+    def un_register_task(self, node_id, task_id)
+        pass
+
