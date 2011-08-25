2fa350703139b9a13bf51580c7d701262c083531
Guy John <d.guyjohn@gmail.com>
2011-08-25 11:56:12 -0400

    Task is now much more minimal
    Will probablly be larger by the end tho

--- libs/PovTask.py
+++ libs/PovTask.py
@@ -1,33 +1,17 @@
 
 class PovTask():
 
-    job_id   = ''
-    task_id  = ''
-    pov_file = ''
-    output   = ''
-    width    = 0
-    height   = 0
-    start    = 0
-    end      = 0
-    finished = True
+    job_id     = ''
+    task_id    = ''
+    task_data  = {}
+    final_data = {}
 
-    def __str__(self):
-        info = (self.job_id, self.task_id, self.width, self.height, self.start, self.end)
-        return "%s-%s: %s,%s,  %s,%s" % (info)
+    task_data['pov_file'] = ''
+    task_data['output']   = ''
+    task_data['width']    = 0
+    task_data['height']   = 0
+    task_data['start']    = 0
+    task_data['end']      = 0
 
-    def store_pov_file(self):
-        #pov file gets uploaded and stored somewhere
-        #(amazon S3 for example)
-        pass
-
-    def get_pov_file(self):
-        pass
-
-    def store_output_file(self):
-        pass
-
-    def get_output_file(self):
-        return self.output
-
-
+    final_data['file']    = ''
 
