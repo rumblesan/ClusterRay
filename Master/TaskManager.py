7f4c4f3fe41b7d2fa97c48753549d2a414c31619
Guy John <d.guyjohn@gmail.com>
2011-08-25 15:24:35 -0400

    Adding stubbed out TaskManager

---
+++ Master/TaskManager.py
@@ -0,0 +1,21 @@
+
+class TaskManager():
+
+    running_tasks  = []
+    finished_tasks = []
+
+    def __init__(self):
+        pass
+
+    def new_tasks(self, task_list):
+        pass
+
+    def get_task(self)
+        pass
+
+    def finished_tasks(self):
+        pass
+
+    def get_finished_tasks(self):
+        pass
+
